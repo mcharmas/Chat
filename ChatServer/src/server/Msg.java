a731ebb89fbd4fa9555e0549c8da2ef1b91bb0d9
Michał Charmas <michal.charmas@gmail.com>
2010-04-15 15:50:03 +0200

    added msg

---
+++ ChatServer/src/server/Msg.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package server;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author orbit
+ */
+public class Msg implements Serializable{
+    Boolean login;
+    Boolean userList;
+    ArrayList<String> list = null;
+    String message=null;
+    String to=null;
+    String from=null;
+
+    public Msg() {
+        this.login = false;
+        this.userList = false;
+    }
+
+    public Msg(Boolean login, Boolean userList) {
+        this.login = login;
+        this.userList = userList;
+        if(userList) {
+            list = new ArrayList<String>();
+        }
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void addToUserList(String user) {
+        if(this.list != null) {
+            this.list.add(user);
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+
+    public Boolean getLogin() {
+        return login;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public Boolean getUserList() {
+        return userList;
+    }
+}
