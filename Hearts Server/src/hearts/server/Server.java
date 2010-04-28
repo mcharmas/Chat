ebbf4bc1f5877def9f8da43e5886d85db6e78e97
Michał Charmas <michal@charmas.pl>
2010-04-28 15:31:00 +0200

    zaimplementowane podstawowe interfejsy serwera i clientow serwera

--- Hearts Server/src/hearts/server/Server.java
+++ Hearts Server/src/hearts/server/Server.java
@@ -4,9 +4,13 @@
  */
 package hearts.server;
 
+import hearts.defs.actions.Action;
+import hearts.defs.actions.ActionListener;
 import hearts.defs.protocol.ServerSocket;
+import hearts.defs.state.GameConstants;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,11 +18,14 @@ import java.util.logging.Logger;
  *
  * @author orbit
  */
-public class Server extends Thread {
+public class Server implements ServerSocket {
 
     private int port;
     private String host;
     private java.net.ServerSocket socket = null;
+    private ArrayList<ServerClient> clientsList = null;
+
+    private ArrayList<ActionListener> listeners = null;
 
     /**
      * <p>Tworzy obiekt serwera, który później trzeba wystartować.</p>
@@ -31,13 +38,25 @@ public class Server extends Thread {
         this.host = host;
 
         this.socket = new java.net.ServerSocket(port);
+        clientsList = new ArrayList<ServerClient>();
+        listeners = new ArrayList<ActionListener>();
     }
 
-    @Override
+    /**
+     * Metoda nasłuchuje w oczekiwaniu na klientów.
+     * Dla każdego nowego klienta tworzny obiekt ServerClient,
+     * dodaje go do list i startuje wątek nasłuchujący.
+     */
     public void run() {
         try {
             while (true) {
                 Socket s = this.socket.accept();
+                Logger.getLogger(Server.class.getName()).log(Level.INFO, "Ktoś się połączył.");
+                ServerClient sc = new ServerClient(s);
+                clientsList.add(sc);
+                Thread th = new Thread(sc);
+                th.start();
+
             }
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.INFO, "IOException on listening for clients.", ex);
@@ -50,5 +69,23 @@ public class Server extends Thread {
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, "Error closing socket.", ex);
             }
         }
+    }
+
+    public int getId() {
+        return GameConstants.SERVER;
+    }
+
+    public void addActionListener(ActionListener listener) {
+        listeners.add(listener);
+    }
+
+    public void notifyListeners(Action action) {
+        for(ActionListener listener: listeners) {
+            listener.actionReceived(action);
+        }
+    }
+
+    public void actionReceived(Action a) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- Hearts Server/src/hearts/server/ServerClient.java
+++ Hearts Server/src/hearts/server/ServerClient.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package hearts.server;
 
 import hearts.defs.actions.Action;
@@ -12,17 +11,22 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author orbit
  */
-public class ServerClient extends Thread implements UserSocket{
-    private Socket socket;
-    private ObjectInputStream input=null;
-    private ObjectOutputStream output=null;
+public class ServerClient implements UserSocket {
 
+    private Socket socket;
+    private ObjectInputStream input = null;
+    private ObjectOutputStream output = null;
+    private ArrayList<ActionListener> listeners = null;
     private int id;
+    private String name = "";
 
     /**
      * <p>Tworzy nowego klienta serwera. Obiekt nasłuchuje na sockecie klienta.</p>
@@ -34,12 +38,33 @@ public class ServerClient extends Thread implements UserSocket{
 
         input = new ObjectInputStream(socket.getInputStream());
         output = new ObjectOutputStream(socket.getOutputStream());
+
+        listeners = new ArrayList<ActionListener>();
+        Logger.getLogger(ServerClient.class.getName()).log(Level.INFO, "ServerClient utworzony.");
     }
 
-    @Override
     public void run() {
-        while(true) {
-
+        try {
+            while (true) {
+                Action action = (Action) input.readObject();
+                if (action == null) {
+                    break;
+                }
+                Logger.getLogger(ServerClient.class.getName()).log(Level.INFO, "Otrzymano akcję.");
+                notifyListeners(action);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServerClient.class.getName()).log(Level.SEVERE, "Klient mógł się rozłączyć.", ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ServerClient.class.getName()).log(Level.SEVERE, "Błąd odbierania danych.", ex);
+        } finally {
+            try {
+                input.close();
+                output.close();
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ServerClient.class.getName()).log(Level.SEVERE, "Błąd zamykania socketów.", ex);
+            }
         }
     }
 
@@ -52,10 +77,46 @@ public class ServerClient extends Thread implements UserSocket{
     }
 
     public void addActionListener(ActionListener listener) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.listeners.add(listener);
     }
 
     public void actionReceived(Action a) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            this.output.writeObject(a);
+            this.output.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerClient.class.getName()).log(Level.SEVERE, "Bład wysyłania akcji.", ex);
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Powiadamia actionListenerów.
+     * @param action
+     */
+    public void notifyListeners(Action action) {
+        for (ActionListener listener : this.listeners) {
+            listener.actionReceived(action);
+        }
+    }
+
+    /**
+     * Rozłącza użytkownika przez brutalne zamknięcie socketa i spowodowanie wyjątku w pętli głowej wątku.
+     */
+    public void disconnect() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServerClient.class.getName()).log(Level.SEVERE, "Bład zamykania socketu przy rozłączniu użytkownika.", ex);
+        }
     }
 }
